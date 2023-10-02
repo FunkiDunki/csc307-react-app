0c3645518fb958ad7295854417703756366baf71
FunkiDunki <nicholashotelling@gmail.com>
2023-10-01 20:13:54 -0700

    added submittions to form

--- react-frontend/src/Form.js
+++ react-frontend/src/Form.js
@@ -2,7 +2,7 @@ import React, {useState} from 'react';
 
     
 
-function Form(){
+function Form(props){
     const [person, setPerson] = useState(
         {
             name: "",
@@ -23,13 +23,18 @@ function Form(){
         }
     }
 
+    function submitForm() {
+        props.handleSubmit(person);
+        setPerson({name: '', job: ''});
+    }
+
     return (
-        <form>
+        <><form>
             <label htmlFor='name'>Name</label>
-            <input type='text' name='name' id='name' value={person.name} onChange={handleChange}/>
+            <input type='text' name='name' id='name' value={person.name} onChange={handleChange} />
             <label htmlFor='job'>Job</label>
-            <input type='text' name='job' id='job' value={person.job} onChange={handleChange}/>
-        </form>
+            <input type='text' name='job' id='job' value={person.job} onChange={handleChange} />
+        </form><input type='button' value='Submit' onClick={submitForm} /></>
     )
 }
 
